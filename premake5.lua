5e82ab5e3ce524241be34357d9eee3bb4bcaecd0
abaurens <abaurens@student.42.fr>
2024-05-26 16:53:23 +0200

    Premake toolchain & removed examples & tests

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+-- spdlog
+project "spdlog"
+  kind "StaticLib"
+  language "C++"
+  cppdialect "C++20"
+  staticruntime "on"
+  systemversion "latest"
+
+
+  targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("%{wks.location}/build/" .. outputdir .. "/%{prj.name}")
+
+  IncludeDir["spdlog"] = "%{wks.location}/libs/spdlog/include"
+
+  includedirs {
+    "%{IncludeDir.spdlog}"
+  }
+
+  defines {
+    "SPDLOG_COMPILED_LIB",
+    "SPDLOG_WCHAR_TO_UTF8_SUPPORT",
+    "SPDLOG_UTF8_TO_WCHAR_CONSOLE",
+    "SPDLOG_WCHAR_FILENAMES"
+  }
+
+  files {
+    "premake5.lua",
+
+    "include/**.h",
+
+    "src/spdlog.cpp",
+    "src/stdout_sinks.cpp",
+    "src/color_sinks.cpp",
+    "src/file_sinks.cpp",
+    "src/async.cpp",
+    "src/cfg.cpp"
+  }
